29fe218851f3cbe037d6f73d2814a5560a72ff33
LAPTOP-JAVTUIF2\user <shcopy99@gmail.com>
2024-12-13 11:02:49 +0800

    更新第一題答案

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -8,12 +8,21 @@
 -- 1. 用戶資料，資料表為 USER
 
 -- 1. 新增：新增六筆用戶資料，資料如下：
---     1. 用戶名稱為`李燕容`，Email 為`lee2000@hexschooltest.io`，Role為`USER`
---     2. 用戶名稱為`王小明`，Email 為`wXlTq@hexschooltest.io`，Role為`USER`
+--     1. 用戶名稱為`李燕容', 'lee2000@hexschooltest.io', 'USER`
+--     2. 用戶名稱為`王小明`，Email 為`wXlTq@hexschooltest.io', 'USER`
 --     3. 用戶名稱為`肌肉棒子`，Email 為`muscle@hexschooltest.io`，Role為`USER`
 --     4. 用戶名稱為`好野人`，Email 為`richman@hexschooltest.io`，Role為`USER`
 --     5. 用戶名稱為`Q太郎`，Email 為`starplatinum@hexschooltest.io`，Role為`USER`
 --     6. 用戶名稱為 透明人，Email 為 opacity0@hexschooltest.io，Role 為 USER
+
+INSERT INTO "USER" (name, email, role) 
+VALUES
+('李燕容', 'lee2000@hexschooltest.io', 'USER'),
+('王小明', 'wXlTq@hexschooltest.io', 'USER'),
+('肌肉棒子', 'muscle@hexschooltest.io', 'USER'),
+('好野人', 'richman@hexschooltest.io', 'USER'),
+('Q太郎', 'starplatinum@hexschooltest.io', 'USER'),
+('透明人', 'opacity0@hexschooltest.io', 'USER')
 
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
 
