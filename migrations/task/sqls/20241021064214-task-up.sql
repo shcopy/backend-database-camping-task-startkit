70adbbb5bc14621f520c5fcd91343cfad577cf98
LAPTOP-JAVTUIF2\user <shcopy99@gmail.com>
2024-12-13 11:34:59 +0800

    UPDATE

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -22,7 +22,7 @@ VALUES
 ('肌肉棒子', 'muscle@hexschooltest.io', 'USER'),
 ('好野人', 'richman@hexschooltest.io', 'USER'),
 ('Q太郎', 'starplatinum@hexschooltest.io', 'USER'),
-('透明人', 'opacity0@hexschooltest.io', 'USER')
+('透明人', 'opacity0@hexschooltest.io', 'USER');
 
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
 UPDATE "USER" 
